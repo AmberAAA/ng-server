e325d2c490ae7d4f6208fb6ebf030de27ce5b503
AmberAAA <anborong@outlook.com>
2019-11-05 13:43:20 +0800

    release user model .

--- src/main/java/top/anborong/server/controller/UserController.java
+++ src/main/java/top/anborong/server/controller/UserController.java
@@ -25,44 +25,45 @@ public class UserController {
 
     private Logger logger = LoggerFactory.getLogger(UserController.class);
 
-    @PostMapping
-    public ResponseData signin (@RequestBody() User user,
-                                HttpServletResponse response
-    ) {
-        logger.info(String.valueOf(user));
-        try {
-            repository.saveAndFlush(user);
-            return ResponseDataUtils.buildSuccess(this.login(user.getName(), user.getPassword(), response));
-        } catch (Exception e) {
-            return  ResponseDataUtils.buildLogicError("用户已存在，请直接登录");
-        }
-    }
 
-    @GetMapping
+//    public ResponseData signin (@RequestBody() User user,
+//                                HttpServletResponse response
+//    ) {
+//        logger.info(String.valueOf(user));
+//        try {
+//            repository.saveAndFlush(user);
+//            return ResponseDataUtils.buildSuccess(this.login(user.getName(), user.getPassword(), response));
+//        } catch (Exception e) {
+//            return  ResponseDataUtils.buildLogicError("用户已存在，请直接登录");
+//        }
+//    }
+
+    @GetMapping("/login")
     public ResponseData login (@RequestParam(value = "name", required = true) String name,
                                @RequestParam(value = "password", required = true) String password,
                                HttpServletResponse response
     ) {
         User user = repository.findByName(name);
-        if (user != null && user.getPassword().equals(password)) {
-            String accToken = TokenUtils.createJwtToken(user.getId() + "");
-//            response.setHeader("ACCESS-TOKEN", accToken);
-            Cookie cookie = new Cookie(SystemEnums.ACCESS_TOKEN.getName(), accToken);
-            response.addCookie(cookie);
-            return ResponseDataUtils.buildSuccess(user);
-        } else {
+        if (user != null && !user.getPassword().equals(password)) {
             return ResponseDataUtils.buildLogicError("账户或密码错误");
         }
+        if (user == null) {
+            user = this.repository.save(new User(name, password));
+            logger.info(user.toString());
+        }
+        String accToken = TokenUtils.createJwtToken(user.getId() + "");
+        Cookie cookie = new Cookie(SystemEnums.ACCESS_TOKEN.getName(), accToken);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        return ResponseDataUtils.buildSuccess(user);
     }
 
-    @GetMapping("/test")
-    @LoginRequired
-    public ResponseData test (HttpServletRequest request) {
-        return ResponseDataUtils.buildSuccess(request.getAttribute(SystemEnums.CURRENT_USER_ID.getName()));
-    }
-
-    @GetMapping("/test1")
-    public ResponseData test1 () {
-        return ResponseDataUtils.buildSuccess("测试1");
+    @GetMapping("/logout")
+    public ResponseData logout (HttpServletResponse response) {
+        Cookie cookie = new Cookie(SystemEnums.ACCESS_TOKEN.getName(), null);
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        return ResponseDataUtils.buildSuccess();
     }
 }
